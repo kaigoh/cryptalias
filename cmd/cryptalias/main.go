ffc350acab98f10c0f4a75c492343d5d45b2a330
Kai Gohegan <kai@gohegan.uk>
2026-01-27 19:29:29 +0000

    Fixed root check

--- cmd/cryptalias/main.go
+++ cmd/cryptalias/main.go
@@ -3,18 +3,13 @@ package main
 import (
 	"log"
 	"os"
-	"os/user"
 
 	"github.com/kaigoh/cryptalias/internal/cryptalias"
 )
 
 func main() {
 	// We DON'T want to be running as root...
-	user, err := user.Current()
-	if err != nil {
-		log.Fatalf("Unable to check user account we're running under! %s", err)
-	}
-	if user.Username == "root" || user.Username == "Administrator" {
+	if os.Getuid() == 0 {
 		log.Fatalf("Don't run cryptalias as root!")
 	}
 
